07a28d54c4250943167999286b26bb3d7b0db3a5
sachin08sachin <sgsachin359@gmail.com>
2022-05-23 15:55:51 +0530

    rct01

---
+++ rct-101-e1-main/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import counter from "./components/Counter/Counter"
+
+function App() {
+  return <div>{/* Code Here */}
+  <counter />
+  </div>;
+}
+
+export default App;
